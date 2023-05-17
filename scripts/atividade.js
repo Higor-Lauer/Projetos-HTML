616317a2c5ecb0f4fab115aaecb69b91b4c7147d
Higor-Lauer <lauerhigor@gmail.com>
2023-05-16 22:09:06 -0400

    ass

---
+++ scripts/atividade.js
@@ -0,0 +1,52 @@
+/*
+var n1 = parseFloat(prompt('Informe o primeiro valor:'));
+var n2 = parseFloat(prompt('Informe o segundo valor:'));
+var n3 = parseFloat(prompt('Informe o terceiro valor:'));
+var n4 = parseFloat(prompt('Informe o quarto valor:'));
+
+result.innerHTML = (n1+n2+n3+n4)/4
+*/
+
+/*
+var saldo = parseFloat(prompt('Informe o valor do depósito:'));
+
+result.innerHTML = saldo*1.02
+*/
+
+/*
+var minuto = Math.floor(252/60);
+var segundo = 252%60;
+
+alert(minuto + ':' + segundo);
+*/
+
+/*
+var n1 = parseInt(prompt('Informe os minutos:'));
+var horastemp = Math.floor(n1/60);
+var dia = Math.floor(horastemp/24);
+var hora = (horastemp%24);
+var minuto = (n1-((dia*1440)+(hora*60)));
+
+document.write('Dias: '+dia+' Horas:'+hora+' Minutos:'+minuto);
+*/
+
+/*
+var dia = parseInt(prompt('Informe os dias:'));
+var hora = parseInt(prompt('Informe as horas:'));
+var minuto = ((dia*1440)+(hora*60));
+
+document.write('Minutos:'+minuto);
+*/
+
+/*
+var raio = parseInt(prompt('Informe o raio da circunferência:'));
+var pi = Math.PI
+
+document.write('Circunferência: '+ 2*raio*pi);
+*/
+
+/*
+var polegada = parseInt(prompt('Informe as polegadas:'));
+
+document.write('Milímetros: '+ polegada*25.4);
+*/
